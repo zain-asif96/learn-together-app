0da7827cd1b0b591ef27f581348a84ff7f300325
zain-asif-96 <zain.raza199699@gmail.com>
2025-01-30 14:14:58 +0500

    feat: article app completed

--- app/src/main/java/com/example/learningtogetherapp/MainActivity.kt
+++ app/src/main/java/com/example/learningtogetherapp/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learningtogetherapp.ui.theme.LearningTogetherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +30,73 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LearningTogetherAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            LearningTogetherAppTheme  {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LearningArticleScreen()
                 }
             }
         }
     }
 }
 
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun LearningArticleScreen(){
+    LearningArticleCard(
+        bannerImage = painterResource(id = R.drawable.bg_compose_background),
+        title = stringResource(R.string.article_title),
+        shortDesc = stringResource(R.string.short_description),
+        longDesc = stringResource(R.string.long_description)
     )
+}
+
+@Composable
+private fun LearningArticleCard(
+    bannerImage: Painter,
+    title: String,
+    longDesc: String,
+    shortDesc: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Image(
+            painter = bannerImage,
+            contentDescription = "Jetpack Compose Banner"
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = modifier
+                .padding(16.dp)
+        )
+        Text(
+            text = shortDesc,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp
+                )
+            )
+        Text(
+            text = longDesc,
+            textAlign = TextAlign.Justify,
+            modifier = modifier
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LearningArticleScreenPreview() {
     LearningTogetherAppTheme {
-        Greeting("Android")
+        LearningArticleScreen()
     }
 }
